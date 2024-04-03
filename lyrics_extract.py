254d03e281eb70b1588ea98e87904ad7321a1f95
Reynold Clare <reynoldclare02@gmail.com>
2024-04-03 22:54:49 +0530

    python: Create Extract Script
    - This is only for English lyrics.

---
+++ lyrics_extract.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import os
+
+def extract_lyrics_from_html(html_file):
+
+    with open(html_file, 'r', encoding='utf-8') as f:  
+        html = f.read()
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    lyrics_elements = soup.find_all('pre')
+
+    lyrics = '\n'.join(lyric.get_text().strip() for lyric in lyrics_elements)
+    return lyrics
+
+def process_html_file(html_file):
+
+    lyrics = extract_lyrics_from_html(html_file)
+
+    base_filename = os.path.splitext(html_file)[0] 
+    json_filename = base_filename + '.json' 
+
+    data = {
+        'lyrics': lyrics
+    }
+
+    with open(json_filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+# Input files folder
+html_dir = 'html_files'
+
+for filename in os.listdir(html_dir):
+    if filename.endswith('.html'):
+        html_filepath = os.path.join(html_dir, filename)
+        process_html_file(html_filepath)
